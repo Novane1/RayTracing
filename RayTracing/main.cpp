3ac8d633da73247fbab4e3429de5702abaf54f1b
Novane1 <nathan.baulard@gmail.com>
2024-03-08 11:33:32 +0100

    Commit section 6

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 #include <fstream>
-#include "vec3.h"
+#include <float.h>
+#include "sphere.h"
+#include "hitablelist.h"
+
 using namespace std;
+
+// Previous useful function
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
+float hit_sphere_float(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else
+	{
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
+
+}
+
+
+//End of this section
+vec3 color(const ray& r,hitable *world) {
+
+	hit_record rec;
+	if (world->hit(r, 0.0,FLT_MAX , rec)) {
+		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	}
+	else{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5 * (unit_direction.y() + 1.0);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+	}
+	
+	
+}
 
 int main() {
 
@@ -9,19 +55,31 @@ int main() {
 	int ny = 100;
 	ofstream outputFile("output.ppm");
 	outputFile << "P3\n" << nx << " " << ny << "\n255\n";
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal (4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+	hitable *list[2];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable* world = new hitable_list(list, 2);
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
-			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			vec3 p = r.point_at_parameter(2.0);
+			vec3 col = color(r,world);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
 			
 			outputFile << ir << " " << ig << " " << ib << endl;
-			
+			if (ir < 0 || ig < 0 || ib < 0) {
+				cout << ir << " " << ig << " " << ib << endl;
+			}
 		}
 	}
-	vec3 B(1.0f, 2.0f, 3.0f);
-	float t = 2.0f;
 	
 	outputFile.close();
 }
